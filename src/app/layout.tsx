df45103798d59450cbf87c496bcd592696a49aca
inthrakumar <inthrak04@gmail.com>
2024-07-30 21:02:12 +0530

    Add about btn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Press_Start_2P } from "next/font/google";
 import "./globals.css";
-import 'nes.css/css/nes.min.css'
 const Press_start = Press_Start_2P({ weight: '400', subsets: ["latin"] });
 
 export const metadata: Metadata = {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client"
+import 'nes.css/css/nes.min.css'
 
 import { useRef, useEffect } from 'react';
 import Link from 'next/link';
 import { Atomic_Age } from 'next/font/google';
 import localFont from 'next/font/local';
 import Typed from 'typed.js';
-
+import bg from '../../public/retro-bg.png';
 
 const font = Atomic_Age({ weight: '400', subsets: ["latin"] });
 const Nametext = localFont({ src: '../font/fifties_movies/Fifties Movies.woff2' });
@@ -21,37 +22,53 @@ export default function Home() {
     });
 
     return () => {
-
       typed.destroy();
     };
   }, []);
 
-
   return (
-    <main className="bg-no-repeat bg-cover min-h-screen flex flex-col">
+    <main
+      className="min-h-screen flex flex-col bg-cover bg-no-repeat"
+      style={{ backgroundImage: `url(${bg.src})` }}
+    >
       <section className="w-full mt-5 mr-7">
-        <div className="w-full flex gap-3 items-center justify-end">
-          <div>
+        <div className="w-full flex gap-3 max-sm:gap-1 items-center justify-end">
+
+
+          <Link href="/about">
             <button type="button" className="nes-btn is-error max-sm:px-[1px] max-sm:py-[2px] max-sm:text-[6px]">
-              <Link href={'/leetcode'} className='text-white'>LeetCode</Link>
+              About
             </button>
-          </div>
-          <div>
+          </Link>
+
+
+          <Link href="/leetcode">
             <button type="button" className="nes-btn is-error max-sm:px-[1px] max-sm:py-[2px] max-sm:text-[6px]">
-              <Link href={'/projects'} className='text-white'>Projects</Link>
+              LeetCode
             </button>
-          </div>
-          <div>
+          </Link>
+
+
+          <Link href="/projects">
             <button type="button" className="nes-btn is-error max-sm:px-[1px] max-sm:py-[2px] max-sm:text-[6px]">
-              <Link href={'/skills'} className='text-white'>Skills</Link>
+              Projects
             </button>
-          </div>
+          </Link>
+
+          <Link href="/skills">
+            <button type="button" className="nes-btn is-error max-sm:px-[1px] max-sm:py-[2px] max-sm:text-[6px]">
+              Skills
+            </button>
+          </Link>
         </div>
+
       </section>
       <section className="mt-5 w-full flex-grow flex items-center justify-center max-sm:mt-3">
         <div className="bg-white nes-container is-centered is-rounded flex flex-col gap-7">
           <p className={`text-6xl max-sm:text-3xl ${Nametext.className}`}>INTHRAKUMAR</p>
-          <p className={`w-56 text-left max-sm:text-2xl ${font.className} text-[2rem]`}> <span className='text-nowrap' ref={el} ></span></p>
+          <p className={`w-56 text-left max-sm:text-2xl ${font.className} text-[2rem]`}>
+            <span className='text-nowrap' ref={el}></span>
+          </p>
         </div>
       </section>
     </main>
