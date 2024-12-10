5b4bdbc3d7c263412f5f26be4009bbb8ff1928e4
inthrakumar <inthrakumar@ik.edu>
2024-12-10 12:45:19 +0530

    add cool mode effect for the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import gsap from 'gsap';
+import { CoolMode } from './ui/cool-mode';
 function Header() {
     const mario = useRef(null);
     const dino = useRef(null);
@@ -31,38 +32,47 @@ function Header() {
         };
         handleScroll();
         window.addEventListener('scroll', handleScroll);
-        
+
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
     }, []);
 
     return (
-        <section className="w-full mt-5 mr-7">
+        <section className="w-full mt-10 mr-20">
             <section className={`icon-list flex w-fit gap-4 max-lg:gap-1 fixed top-4 left-2 max-sm:hidden ${isScrolled ? 'hidden' : ''}`}>
                 <i className="nes-mario" ref={mario}></i>
                 <i className="nes-charmander" ref={dino}></i>
             </section>
             <div className="w-full flex gap-3 max-sm:gap-1 items-center justify-end">
                 <Link href="/">
-                    <button type="button" className="nes-btn is-error max-sm:px-[1px] max-sm:py-[2px] max-sm:text-[6px]">
-                        Home
-                    </button>
+                    <CoolMode>
+
+                        <button type="button" className="nes-btn is-error max-sm:px-[1px] max-sm:py-[2px] max-sm:text-[6px]">
+                            Home
+                        </button>
+                    </CoolMode>
                 </Link>
                 <Link href="/leetcode">
-                    <button type="button" className="nes-btn is-error max-sm:px-[1px] max-sm:py-[2px] max-sm:text-[6px]">
-                        LeetCode
-                    </button>
+                    <CoolMode>
+                        <button type="button" className="nes-btn is-error max-sm:px-[1px] max-sm:py-[2px] max-sm:text-[6px]">
+                            LeetCode
+                        </button>
+                    </CoolMode>
                 </Link>
                 <Link href="/projects">
-                    <button type="button" className="nes-btn is-error max-sm:px-[1px] max-sm:py-[2px] max-sm:text-[6px]">
-                        Projects
-                    </button>
+                    <CoolMode>
+                        <button type="button" className="nes-btn is-error max-sm:px-[1px] max-sm:py-[2px] max-sm:text-[6px]">
+                            Projects
+                        </button>
+                    </CoolMode>
                 </Link>
                 <Link href="/skills">
-                    <button type="button" className="nes-btn is-error max-sm:px-[1px] max-sm:py-[2px] max-sm:text-[6px]">
-                        Skills
-                    </button>
+                    <CoolMode>
+                        <button type="button" className="nes-btn is-error max-sm:px-[1px] max-sm:py-[2px] max-sm:text-[6px]">
+                            Skills
+                        </button>
+                    </CoolMode>
                 </Link>
             </div>
         </section>
